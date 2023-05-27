2b3f52c9d36e359338d7dc5f0bf49abf1b5c532b
Havin_proud <havin.becca.93@gmail.com>
2023-05-27 14:28:37 -0500

    finished

--- practice-bubble-sort-master/bubble-sort.js
+++ practice-bubble-sort-master/bubble-sort.js
@@ -1,30 +1,27 @@
 
 function bubbleSort(arr) {
-  if (arr.length === 1) {
-    console.log(arr.join(","));
-    return;
+  let hasSwapped = false;
+      // Iterate through the array
+
+        // If the current value is greater than its neighbor to the right
+        while(true){
+          // Swap those values
+  for (let i = 0; i <arr.length; i++){
+      if(arr[i] > arr[i+1]){
+      [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+      hasSwapped = true;
+
+          // Do not move this console.log
+          console.log(arr.join(","));
+    }
+    }
+    if(!hasSwapped) return
+    hasSwapped =false
   }
-    // Iterate through the array
-for (let i = 0; i < arr.length; i++){
-  if(arr[i] > arr[i+1]) {
-    let temp = arr[i];
-    arr[i] = arr[i+1];
-    arr[i+1] = temp;
+     // If you get to the end of the array and no swaps have occurred, return
 
-    // bubbleSort(arr)
+      // Otherwise, repeat from the beginning
+
   }
-// console.log(arr)
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
 
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
-}
-}
-
-module.exports = bubbleSort;
+  module.exports = bubbleSort;
